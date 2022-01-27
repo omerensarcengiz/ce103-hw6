9ed2edc496bc60ae62b8ff29f082e748ec058629
Ömer Ensar Cengiz <93315420+omerensarcengiz@users.noreply.github.com>
2022-01-27 07:22:17 +0300

    Add files via upload

---
+++ Calculator Combination/Calculator Combination/Form11.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using calculator;
+using LesGraphingCalc;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator_Combination
+{
+    public partial class CALCULATOR : Form
+    {
+        public CALCULATOR()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form1 form = new Form1();
+            form.ShowDialog();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            CalcForm calcForm = new CalcForm();
+            calcForm.ShowDialog();
+        }
+
+        private void CALCULATOR_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
